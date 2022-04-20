90ebef07fdfeb6903a4d25f8ebdf8ec16106c959
zombital <talhajrasi2004@gmail.com>
2022-04-20 13:25:05 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,25 @@
 
+public class main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(new File("sample.in"));
+		int cities = scan.nextInt();
+		scan.nextLine();
+		
+		for(int x = 0; x<cities; x++) {
+			int m = scan.nextInt();
+			scan.nextLine();
+		
+			graph g = new graph(m);
+			
+			int r = scan.nextInt();
+			scan.nextLine();
+			
+			for(int i = 0; i<r; i++) {
+				g.addEdge(scan.nextInt(), scan.nextInt());
+			}
+			
+		}
+	}
+}
